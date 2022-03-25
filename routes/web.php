def1bae040b34cf3c026d0c980fca8449adb38d0
zurctrebla <albertcruz@terra.com.br>
2022-03-25 10:58:29 -0300

    install laravel/ui, adminlte

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/admin/users', [UserController::class, 'index'])->name('users.index'
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
