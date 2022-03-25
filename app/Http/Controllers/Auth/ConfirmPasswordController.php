1c6ecf19677243a09b0c4fcc7e8b2aa5999df951
zurctrebla <albertcruz@terra.com.br>
2022-03-25 11:35:56 -0300

    auth adjustments

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -26,7 +26,9 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected $redirectTo = '/admin/home';
 
     /**
      * Create a new controller instance.
