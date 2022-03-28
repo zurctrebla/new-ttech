8b3aeff34e25bc9c80afd7f7aaafc0cc113cda30
zurctrebla <albertcruz@terra.com.br>
2022-03-28 10:24:41 -0300

    Products Updated

---
+++ database/migrations/2022_03_28_095431_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->uuid('uuid');
+            $table->string('equipment');
+            $table->string('serial')->nullable();
+            $table->string('tag')->unique();
+            $table->string('model')->unique();
+            $table->string('destiny')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
