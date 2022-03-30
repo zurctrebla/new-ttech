a360de3d8d7234abc64bfb3e231d9054e10e48f6
zurctrebla <albertcruz@terra.com.br>
2022-03-30 13:26:00 -0300

    relationship product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'equipment', 'serial', 'tag', 'model', 'destiny', 'status'];
+
+    /**
+     * Get User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
