0da3c2dbfa23643578923a3f63bdfaf9fbb7faae
zurctrebla <albertcruz@terra.com.br>
2022-03-30 13:28:17 -0300

    adjustment inventory form

--- resources/views/admin/pages/inventories/_partials/form.blade.php
+++ resources/views/admin/pages/inventories/_partials/form.blade.php
@@ -3,39 +3,31 @@
 <div class="row">
     <div class="col-sm-6">
       <div class="form-group">
-       <label>Nome *</label>
-       <input type="text" name="product" class="form-control" placeholder="Nome:" value="{{ $inventory->product ?? old('product') }}" >
+       <label>Equipamento *</label>
+       <input type="text" name="equipment" class="form-control" placeholder="Nome do Equipamento:" value="{{ $inventory->equipment ?? old('equipment') }}" >
       </div>
     </div>
     <div class="col-sm-6">
         <div class="form-group">
-         <label>Descrição *</label>
-         <input type="text" name="description" class="form-control" placeholder="Descrição:" value="{{ $inventory->description ?? old('description') }}" >
+         <label>Condição *</label>
+         <input type="text" name="condition" class="form-control" placeholder="Condição:" value="{{ $inventory->condition ?? old('condition') }}" >
         </div>
     </div>
 </div>
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-4">
       <div class="form-group">
-       <label>Marca *</label>
+       <label>Marca </label>
        <input type="text" name="brand" class="form-control" placeholder="Marca:" value="{{ $inventory->brand ?? old('brand') }}" >
       </div>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-4">
         <div class="form-group">
          <label>Modelo *</label>
          <input type="text" name="model" class="form-control" placeholder="Modelo:" value="{{ $inventory->model ?? old('model') }}" >
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-sm-6">
-      <div class="form-group">
-       <label>Etiqueta *</label>
-       <input type="text" name="tag" class="form-control" placeholder="Marca:" value="{{ $inventory->tag ?? old('tag') }}" >
-      </div>
-    </div>
-    <div class="col-sm-6">
+    <div class="col-sm-4">
         <div class="form-group">
          <label>Quantidade *</label>
          <input type="text" name="amount" class="form-control" placeholder="Quantidade Inicial:" value="{{ $inventory->amount ?? old('amount') }}" >
@@ -44,7 +36,19 @@
 </div>
 <div class="row">
     <div class="col-sm-12">
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" name="export" class="custom-control-input" id="customCheck1">
+            <label class="custom-control-label" for="customCheck1">Exibir na manutenção?</label>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12">
       <div class="form-group">
+        <p>
+            <span class="text-danger">* </span>Campo obrigatório
+        </p>
         <button type="submit" class="btn btn-dark">Enviar</button>
       </div>
     </div>
