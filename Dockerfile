1ecfce3fd20ca83fe12a89e0b6c3190d68ff2541
zurctrebla <albertcruz@terra.com.br>
2022-07-11 21:26:16 -0300

    Dockerfile updated

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,47 @@
-FROM php:7.4-fpm
+# FROM php:7.4-fpm
+FROM php:8.0-fpm-buster
 
 # Arguments defined in docker-compose.yml
 ARG user
 ARG uid
 
 # Install system dependencies
+# RUN apt-get update && apt-get install -y \
+#     git \
+#     curl \
+#     libpng-dev \
+#     libonig-dev \
+#     libxml2-dev \
+#     zip \
+#     libzip-dev zip unzip
 RUN apt-get update && apt-get install -y \
+    build-essential \
     git \
     curl \
     libpng-dev \
+    libjpeg-dev \
+    libfreetype6-dev \
+    libjpeg62-turbo-dev \
+    jpegoptim optipng pngquant gifsicle \
     libonig-dev \
     libxml2-dev \
     zip \
-    libzip-dev zip unzip
-    #unzip
+    sudo \
+    unzip \
+    npm \
+    nodejs \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install -j$(nproc) gd
 
 # Clear cache
 RUN apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Install PHP extensions
-RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd sockets zip
+# RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd sockets zip
+
+# Install PHP extensions
+RUN docker-php-ext-configure gd --enable-gd --with-freetype --with-jpeg
+RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd
 
 # Get latest Composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
--- app/Http/Controllers/AnnotationController.php
+++ app/Http/Controllers/AnnotationController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use GoogleCloudVision\GoogleCloudVision;
 use GoogleCloudVision\Request\AnnotateImageRequest;
 
+use Image;
+//use Intervention\Image\ImageManagerStatic as Image;
+
 class AnnotationController extends Controller
 {
     //show the upload form
@@ -18,8 +21,18 @@ class AnnotationController extends Controller
     {
         if($request->file('image')){
 
-            //convert image to base64
+            //  resize image with Intervention/Image
+            $image = Image::make($request->file('image'))->encode('jpg', 75);
+            return $image->response();
+
+            // $image = Image::make($request->file('image'))->encode('jpg', 10)->resize(200,200);
+            // return $image->response();  //  ok
+            // return $request->file('image');   //  ok
+            // $image->resize(320,320);
+
+            // convert image to base64
             $image = base64_encode(file_get_contents($request->file('image')));
+            // $image = base64_encode(file_get_contents($image));
 
             //prepare request
             $request = new AnnotateImageRequest();
@@ -29,27 +42,18 @@ class AnnotationController extends Controller
 
             //send annotation request
             $response = $gcvRequest->annotate();
-
             // return json_encode(["description" => $response->responses[0]->textAnnotations[0]->description]);
-
             $array = (array) $response->responses[0]->textAnnotations[0]->description;
-
             foreach ($array as $key => $value) {
-
                 $words = explode("\n", $value);
-
                 // dd($words); //  array
-
                 $jogo = $words[0];
                 $entrada = (integer) $words[17];
                 $saida = (integer) $words[20];
-
                 echo "Jogo: " . $jogo . "<br>";
                 echo "Entrada: " . $entrada . "<br>";
                 echo "Saida: " . $saida . "<br>";
-
             }
-
         }
     }
 }
